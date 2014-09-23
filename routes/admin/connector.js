d8273e8f92d56fbcb0bbad468f871a915e7b60e6
badlee <badinga.ulrich@gmail.com>
2014-09-23 10:13:12 +0100

    Update connector.js

--- routes/admin/connector.js
+++ routes/admin/connector.js
@@ -1,242 +1,286 @@
-var path = require('path');
-var fs = require('fs');
-var url = require('url');
-var colors = require('colors');
-var cache = {};
-var md5 = require('MD5');
-var util = require("util");
-var events = require("events");
-var argv = process.argv;
-	argv.shift();
-	argv.shift();
-var getDir = function (dbProdPath){
-	var pwd = process.cwd();
-	if(typeof __dirname != "undefined")
-		pwd = __dirname;
-		
-	return String(dbProdPath).search(/^app:\/\//i) === 0 ? 
-		path.resolve.apply(path,[path.sep].concat(String(dbProdPath).replace(/\//g,path.sep).replace("app:"+(path.sep),pwd).split(path.sep).slice(1)))
-		: dbProdPath
+var Properties = {
+	"kannel.js":{
+		host : { type : String, label : "Kannel Host", help : "Defaut : 127.0.0.1", default : "127.0.0.1" },
+		port : { type: String  , label : "Kannel Port", help : "Defaut : 13001", default : 13001},
+		id :     { type: String, label : "Identifiant de la smsbox", help : "Defaut : LoveIsMyReligion", default : "LoveIsMyReligion" },
+		tls : {type : Boolean, label :'Secure',default:  false, inputType : 'checkbox'}
+	},
+	"shorty.js" : {
+        "mode" : {type : String, label :"Mode",default:"transceiver", help : "Defaut : transceiver"},
+        "host" : { type : String, label : "Host", help : "Defaut : 127.0.0.1", default : "127.0.0.1" },
+		"port" : { type: String  , label : "Port", help : "Defaut : 2775", default : 2775},
+		"system_id":    { type: String, label : "Identifiant", help : "Defaut : username", default : "username" },
+		"password":     { type: String, label : "Password", help : "Defaut : password", default : "password" },
+        "system_type":  { type: String, label : "Type", help : "Defaut : SMPP", default : "SMPP" },
+        "addr_ton":     { type: Number, label : "TON", help : "Defaut : 0", default : "0" },
+        "addr_npi":     { type: Number, label : "NPI", help : "Defaut : 1", default : 1 },
+        "addr_range":   { type: String, label : "RANGE", help : "Defaut : **Empty**", default : "" },
+        "timeout":      { type: Number, label : "Timeout", help : "Defaut : 30", default : 30 },
+        "client_keepalive":  {type : Boolean, label :'Keepalive',default:  false, inputType : 'checkbox'},
+        "client_reconnect_interval": { type: Number, label : "Reconnection", help : "Defaut : 2500", default : 2500 },
+        "strict":       {type : Boolean, label :'Strict',default:  true, inputType : 'checkbox'}
+    },
+    "smpp.js" : {
+    	"host" : { type : String, label : "Host", help : "Defaut : 127.0.0.1", default : "127.0.0.1" },
+		"port" : { type: String  , label : "Port", help : "Defaut : 2775", default : 2775},
+		"system_id":    { type: String, label : "Identifiant", help : "Defaut : username", default : "username" },
+		"password":     { type: String, label : "Password", help : "Defaut : password", default : "password" }
+    }
 }
-		
-function Connector() {
-    events.EventEmitter.call(this);
-	for(var j = (settings.maxPool || 5); j--;){
-		VMs[j] = require('child_process').fork(path.resolve(__dirname,'vm.js'),argv);
-		VMs[j].send({type:"setDIR", data:__dirname});
-		VMs[j].send({type:"settings", data:settings});
-		VMs[j].on('message', (function(m) {
-			if (m.type === 'sms'){
-				this.sendSMS(m,new Buffer(m.receiver).toString());
+module.exports = function(app,dir){
+	var hidden = {id:1,conf : 1,},
+		readOnly={id:1,name:1},
+		liens = {
+			"connector.list" : "Liste des Connectors"
+		};
+	app.route(/^\/connector\.json(\/([^\/]+)?)?$/i)
+		.get(function(req,res,next){
+			var id = req.params[1] || false;
+			Models.Connector[ id ? 'findOne' : 'find' ](id? {where : {id : id} } : {},function(err,connectors){
+				if(err || !connectors)
+					return res.json({success:false, message : err.message});
+
+				res.json({success : true, data: (connectors.length ? connectors.map(function(item){  return { value : item.num , text: "["+item.num + "] " +item.desc}}) : connectors),message:"Requete Ok"})
+			})
+		})
+	app.route("/connector.list")
+		.get(function(req,res,next){
+			if(! ("connector" in req.user.droits)){
+				res.status(403);
+				return res.render("page-error",{error : {code:403, message : "Acces interdit"}});
 			}
-		}).bind(this));
-	}
-}
-util.inherits(Connector, events.EventEmitter);
-
-var VMs = {};
-
-process.on('exit', function(){
-	for(var i in VMs)
-		VMs[i].kill();
-});
-/* load models */
-Models = {};
-settings = require(path.resolve(__dirname,"settings.json"))
-var	caminte = require('caminte'),
-    Schema = caminte.Schema,
-    db = {
-         driver     :  settings.dbType || "memory",
-         host       : settings.dbHost || "",
-         port       : settings.dbPort || "",
-         username   : settings.dbUser || "",
-         password   : settings.dbPwd || "",
-         database   : settings.dbPath ? getDir(settings.dbPath) :  "",
-         pool       : settings.dbPool || false // optional for use pool directly 
-    },
-    dbProd = {
-         driver     :  settings.dbProdType || "memory",
-         host       : settings.dbProdHost || "",
-         port       : settings.dbProdPort || "",
-         username   : settings.dbProdUser || "",
-         password   : settings.dbProdPwd || "",
-         database   : settings.dbProdPath ?  getDir(settings.dbProdPath) :  "",
-         pool       : settings.dbProdPool || false // optional for use pool directly 
-    },
-	schema = new Schema(db.driver, db),
-    schemaProd = new Schema(dbProd.driver, dbProd);
-
-fs.readdirSync(path.join(__dirname,"models")).forEach(function(route){
-	require(path.join(__dirname,"models",route))(schema,schemaProd);
-});
-
-
-Object.defineProperties(Connector.prototype, {
-	execScript : {
-		value: function(script,sms,keyword) {
-			this.currVM = this.currVM+1 >= (settings.maxPool||5) ? 0 :  this.currVM+1;
-			sms.type = "sms";
-			sms.file = script;
-			sms.keywords = keyword;	
-			VMs[this.currVM].send(sms);
-		},
-		writable: false,
-		enumerable: false,
-		configurable: false
-	},
-	currVM : {
-		value: -1,
-		writable: true,
-		enumerable: false,
-		configurable: false
-	},
-	sendSMS  : {
-		value: function(data,id){
-			console.log("send SMS",id,data.receiver.toString(),data.sender.toString(),data.msgdata.toString());
-			this.emit("stats++",id);
-			this._sendSMS(data);
-		},
-		writable: false,
-		enumerable: false,
-		configurable: false
-	},
-	_sendSMS  : {
-		value: function(data){
-			try{this.emit("sendSMS",data);}catch(e){}
-			new Models.SMS({ 
-				pdu: data,
-				sms : (data.msgdata || "").toString(),
-				from: data.sender.toString(),
-				to: data.receiver.toString(),
-				SMSC: (data.smsc_id || "").toString(),
-				MotCle : "",
-				success : false,
-				received : false
-			}).save(function(err){
-				//if(err)
-					//console.log(err);
-			});
-		},
-		writable: false,
-		enumerable: false,
-		configurable: false
-	},
-	failSMS  : {
-		value: function(data,raison){
-			console.log("failSMS",raison);	
-			try{this.emit("failSMS",data,raison);}catch(e){};
-			var save = { 
-				pdu: data,
-				sms : (data.msgdata || "").toString(),
-				from: data.sender.toString(),
-				to: data.receiver.toString(),
-				SMSC: data.smsc_id.toString(),
-				MotCle : "",
-				success : false,
-				raison : raison
-			};
-			new Models.SMS(save).save(function(err){
+			
+			var heads = {}, checkbox = {}, listes = {}, listValues = {};
+			for(var i in Models.Connector.properties){
+				if(Models.Connector.properties[i].label)
+					heads[i] = Models.Connector.properties[i].label;
+				if(Models.Connector.properties[i].inputType == 'checkbox')
+					checkbox[i] = 1;
+				else if(Models.Connector.properties[i].list)
+					listes[i] = Models.Connector.properties[i].list;
+				else if(Models.Connector.properties[i].data)
+					listValues[i] = Models.Connector.properties[i].data;
+			}
+			
+			Models.Connector.find({},function(err,items){
 				if(err)
-					console.log(err);
-			});
-		},
-		writable: false,
-		enumerable: false,
-		configurable: false
-	},
-	successSMS : {
-		value : function(data,script,keyword){
-			try{this.emit("successSMS",data);}catch(e){}
-			new Models.SMS({ 
-				pdu: data,
-				sms : (data.msgdata || "").toString(),
-				from: data.sender.toString(),
-				to: data.receiver.toString(),
-				SMSC: data.smsc_id.toString(),
-				MotCle : keyword,
-				script : script,
-				success : true
-			}).save(function(err){
-				//if(err)
-					//console.log(err);
-			});
-		},
-		writable: false,
-		enumerable: false,
-		configurable: false
-	},
-	runSMS : {
-		value : function(data,err,items){
-			var id = (data && data.receiver ? data.receiver : "unknow").toString();
-			console.log("receive SMS",id,data.receiver.toString(),data.sender.toString(),data.msgdata.toString());
-			this.emit("stats--",id);
-			if(err || !items)
-				return this.failSMS(data);
+					return next(err);
+				res.render("build-resposive-table",{ hidden : hidden, listValues : listValues,fields : heads,title:"Liste des connectors",data : items, checkboxs : checkbox, listes : listes,getActions : function(j,user){
+					if(!(j.name in VMs))
+						initVMs(j);
+					return (VMs[j.name].online ?
+							'<a title="Stop server" href="'+dir+'/connector.stop/'+j.name+'" class="btn btn-dark btn-sm"><i class="fa fa-stop"></i>  Stop Server</a>  ' :
+							'<a title="Start server" href="'+dir+'/connector.start/'+j.name+'" class="btn btn-success btn-sm"><i class="fa fa-play"></i>  Start Server</a>  ' 
+						)+
+						'<a title="Edit" href="'+dir+'/connector.info/'+j.id+'" class="btn btn-blue btn-sm"><i class="fa fa-edit"></i></a>  '+
+				    	'<a id="user-remove-'+j.id+'" title="Effacer" href="javascript:exec(\'/admin/connector.remove/'+j.id+'\',\'{1}\',\'Error : {1}\',\'#user-remove-'+j.id+'\')" class="btn btn-red btn-sm"><i class="fa fa-trash-o"></i></a>';
+				}  });
+			})
+		})
+	app.get(/^\/connector.stop\/(.*)$/i,function(req,res){
+		if(! ("connector" in req.user.droits)){
+			res.status(403);
+			return res.render("page-error",{error : {code:403, message : "Acces interdit"}});
+		}
+		if(!(req.params[0] && req.params[0] in VMs)){
+			res.status(404);
+			return res.render("page-error",{error : {code:404, message : "VM not Found"}});
+		}
+		VMs[req.params[0]].send("stop");
+		return res.redirect(dir+"/connector.list");
+	});
+	app.get(/^\/connector.start\/(.*)$/i,function(req,res){
+		if(! ("connector" in req.user.droits)){
+			res.status(403);
+			return res.render("page-error",{error : {code:403, message : "Acces interdit"}});
+		}
+		if(!(req.params[0] && req.params[0] in VMs)){
+			res.status(404);
+			return res.render("page-error",{error : {code:404, message : "VM not Found"}});
+		}
+		
+		VMs[req.params[0]].send({type:"start",data:VMs[req.params[0]].item.conf});
+		return res.redirect(dir+"/connector.list");
+	    
+	});
+	app.route("/connector.add")
+		.all(function(req,res,next){
+			if(!("connector" in req.user.droits))
+				return res.redirect(dir+"/connector.list");
+			next();
+		})
+		.get(function(req,res){
+			var update = {},
+				hidden = {id:1,conf:1};
+			for(var cle in Models.Connector.properties){
+				if(readOnly[cle] || (!req.user.isAdmin && Models.Connector[cle] && Models.Connector[cle].needAdmin) )
+					continue;
+				if(Models.Connector.properties[cle].default)
+					update[cle] = Models.Connector.properties[cle].default;
+			}
+			res.render("build-form",{hidden:hidden,readOnly:{id:1},fields : Models.Connector.properties,title:"Ajouter un connector",data : update});
+		}).post(function(req,res,next){
+			var readOnly = {id:1},
+				update = {
+				};
+			for(var cle in Models.Connector.properties){
+				if(readOnly[cle] || (!req.user.isAdmin && Models.Connector[cle] && Models.Connector[cle].needAdmin) )
+					continue;
+				if(Models.Connector.properties[cle].type == Boolean)
+					update[cle] = false;
+				if( req.body[cle] ){
+					if(Models.Connector.properties[cle].type == Boolean)
+						update[cle] = (req.body[cle] == 'true' || req.body[cle] == 'on' || req.body[cle] == 1);
+					else
+						if(Models.Connector.properties[cle].data){
+							var tmp = {};
+							for(var i in Models.Connector.properties[cle].data)
+								if(req.body[cle] && req.body[cle].indexOf &&	req.body[cle].indexOf(i) != -1 )
+									tmp[i] = 1;
+							update[cle] = tmp;
 
-			var sms = data.msgdata.toString();
-			var keyword = sms.split(" ");
-			var to = data.receiver.toString();
-			if(items[to]) // service binded to shortCode
-				items = items[to];
-			else if(items["*"]) // default service
-				items = items["*"];
-			else
-				return this.failSMS(data,'No route found');
-			/*if(items.length){
-				for(var i in items){
-					if((new RegExp(items[i].keyword,'i')).test(sms)){
-						items = items[i];
-						break;
+						}else
+							update[cle] = req.body[cle]
+				}
+			}
+			Models.Connector.findOne({ where : {name : update.name }}, function(err,connector){
+				if(err)
+					return next(err);
+				if(connector){
+					res.status(403);
+					return res.render("page-error",{error : {code:403, message : "Le Connector "+req.body.name+" Existe deja"}});	
+				}
+				new Models.Connector(update).save(function(err,connector){
+					if(err)
+						return next(err);
+					initVMs(connector);
+					res.redirect(dir+"/connector.info/"+connector.id);
+				})
+			});
+		})
+		app.route(/^\/connector\.info\/([^\/]+)?$/i)
+		.all(function(req,res,next){
+			if(!("connector" in req.user.droits))
+				return req.redirect(dir+"/connector.list");
+			next();
+		})
+		.get(function(req,res,next){
+			var id = req.params[0] || false;
+			if(!id)
+				return req.redirect(dir+"/connector.list");
+			Models.Connector.findOne({where : {id : id}}, function(err, connector){
+				if(err)
+					return next(err);
+				if(connector){
+					var update = {},
+						hidden = {};
+					var properties = Properties[connector.type];
+					if(!properties){
+						res.status(403);
+						return res.render("page-error",{error:{ code:404, message:"Connecteur invalide!!!"}});
+					}
+					var readOnly = {};
+					var data = connector.conf || {};
+					for(var cle in properties){
+						if((properties[cle] && properties[cle].readOnly) || (!req.user.isAdmin && properties[cle] && properties[cle].needAdmin) )
+							readOnly[cle] = 1;
+						else
+							if(properties[cle] && properties[cle].hidden)
+								hidden[cle] = 1;
+
+						if(properties[cle].default)
+							data[cle] == data[cle] || properties[cle].default;
+					}
+					res.render("build-form",{liens:liens,hidden : hidden,readOnly : readOnly,fields : properties,title:"Configurer le Connector : "+connector.name,data : data});
+				}else
+					res.redirect(dir+"/connector.list");
+			});
+		}).post(function(req,res,next){
+			var id = req.params[0] || false;
+			if(!id)
+				return req.redirect(dir+"/connector.list")
+			var readOnly = {},
+				update = {id:id};
+			Models.Connector.findOne({where : {id : update.id  }}, function(err,connector){
+				if(err)
+					return next(err);
+				if(!connector){
+					res.status(404);
+					return res.render("page-error",{error:{ code:404, message:"Connector Not found"}});
+				}
+				var properties = Properties[connector.type];
+					if(!properties){
+						res.status(403);
+						return res.render("page-error",{error:{ code:404, message:"Connector invalide!!!"}});
+					}
+				for(var cle in properties){
+					if(readOnly[cle] || (!req.user.isAdmin && properties[cle] && properties[cle].needAdmin) ){
+						update[cle] = connector.conf[cle] || properties[cle].default;
+						continue;
+					}
+					if(properties[cle].default && !req.body[cle])
+						req.body[cle] = properties[cle].default;
+					if(properties[cle].type == Boolean)
+						update[cle] = properties[cle].default ? properties[cle].default : false;
+					if( req.body[cle] ){
+						if(properties[cle].type == Boolean)
+							update[cle] = (req.body[cle] == 'true' || req.body[cle] == 'on' || req.body[cle] == 1);
+						else
+							if(properties[cle].data){
+								var tmp = {};
+								for(var i in properties[cle].data)
+									if(req.body[cle] && req.body[cle].indexOf &&	req.body[cle].indexOf(i) != -1 )
+										tmp[i] = 1;
+								update[cle] = tmp;
+
+							}else
+								update[cle] = req.body[cle];
 					}
 				}
-			}*/
+				var conf = {}
+				for(var cle in update)
+					conf[cle]  = update[cle];
+				connector.conf = conf;
+				connector.save(function(err,connector){
+					if(err)
+						return next(err);
+					if(connector.name in VMs)
+						VMs[connector.name].item = connector;
+					else
+						initVMs(connector);
+					res.redirect(dir+"/connector.info/"+connector.id);
+				})
+			});
+		});
+		app.route(/^\/connector\.remove\/([^\/]+)$/i)
+		.all(function(req,res,next){
+			if(!("connector" in req.user.droits))
+				return req.redirect(dir+"/connector.list");
+			next();
+		})
+		.get(function(req,res,next){
+			var id = req.params[0] || false;
+			if(!id)
+				return req.redirect(dir+"/connector.list");
+			Models.Connector.findOne({where : {id : req.params[0]  }}, function(err,connector){
+				if(err)
+					return next(err);
+				if(!connector){
+					res.status(404);
+					return res.render("page-error",{error:{ code:404, message:"Connector Not found"}});
+				}
+				Models.Connector.remove({where : {id : req.params[0] }},function(err){
+					if(err)
+						return next(err);
+					console.log(arguments);
+					if(connector.name in VMs)
+						try{
+							VMs[connector.name].kill();
+							delete VMs[connector.name];
+						}catch(e){}
+					res.json({success : true, message : 'Connector effacé'});
+				});
+			});
+		});
+}
 
-			console.log(("receive SMS "+data.id+" MotCle "+keyword[0]).grey);
-			var expression = items["validator-val"].split("[:ø:]");
-			var valid = new RegExp(expression[0] || "",expression[1] || "");
-			if(!valid.test(sms))
-				return this.failSMS(data,"Validation Test Fails");
-			expression = items["blackList-val"].split("[:ø:]");
-			if((new RegExp(expression[0] || "",expression[1] || "")).test(sms))
-				return this.failSMS(data,"blackList Validation Test OK");
-			
-			this.successSMS(data,items.scriptId,keyword[0]);
-			/* reecri le sms */
-			if(items.rewriter){
-				data.msgdata_orig = data.msgdata;
-				sms = sms.replace(valid,items.rewriter);
-				keyword = sms.split(" ");
-				data.msgdata = sms;
-			}
-			data.id = true;
-			data.sender = data.sender.toString();
-			data.receiver = data.receiver.toString();
-			this.execScript(items["scriptId-val"],data,keyword);
-		},
-		writable: false,
-		enumerable: false,
-		configurable: false
-	},
-	execSMS : {
-		value : function(data){
-			/* looking for keyword */
-			var sms = data.msgdata.toString();
-			var keyword = sms.split(" ");
-			var name = path.join(__dirname,"scripts","keywords", md5(keyword[0].toLowerCase()));
-			fs.exists(name,(function (exists) {
-				if(!exists)
-			  		return this.runSMS(data,"Not found");
-			  	fs.readFile(name, (function (err, json) {
-					if(err) return this.runSMS(data,err);
-					this.runSMS(data,null,JSON.parse(json));
-				}).bind(this));
-			}).bind(this));
-		},
-		writable: false,
-		enumerable: false,
-		configurable: false
-	}
-});
-
-
-module.exports = Connector;
